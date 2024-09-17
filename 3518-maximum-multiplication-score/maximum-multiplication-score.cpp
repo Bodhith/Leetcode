1ba2761c93737578db34d6404b70d52520cb72e4
Bodhith <34181215+Bodhith@users.noreply.github.com>
2024-09-17 06:29:03 -0700

    Time: 192 ms (46.41%) | Memory: 185.5 MB (26.00%) - LeetSync

---
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    #define P_MAX 1000000000000000000
+
+    void print_2d_vector(vector<vector<long long int>> &matrix) {
+        for (const auto &row : matrix) {
+            for (const auto &element : row) {
+                cout << element << " ";
+            }
+                cout << endl;
+        }
+    }
+
+    void fill_matrix(vector<int> &a, vector<int>& b, vector<vector<long long int>> &matrix) {
+        int a_n, m_n, i, limit, j;
+
+        a_n = a.size(), m_n = matrix[0].size();
+
+        for(i=0; i<a_n; i++) {
+            limit = m_n - a_n + i + 1;
+            for(j=i; j<limit; j++) {
+                if(!i)  matrix[i][j] = ((j)? max(matrix[i][j-1], (long long int)a[i]*b[j]): ((long long int)a[i]*b[j]));
+                else    matrix[i][j] = max(matrix[i][j-1], (long long int)a[i]*b[j]+matrix[i-1][j-1]);
+            }
+        }
+    }
+
+    long long int maxScore(vector<int>& a, vector<int>& b) {
+        int a_n, b_n;
+        long long int ans;
+        int i;
+
+        a_n = a.size(), b_n = b.size();
+
+        vector<vector<long long>> matrix(a_n, vector<long long>(b_n, (long long)-P_MAX));
+
+        fill_matrix(a, b, matrix);
+
+        // print_2d_vector(matrix);
+
+        for(ans=-P_MAX, i=a_n-1; i<b_n; i++)    ans = max(ans, matrix[a_n-1][i]);
+
+        return ans;
+    }
+};
